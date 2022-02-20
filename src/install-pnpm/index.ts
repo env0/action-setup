4225f250d64e7c5fd868e3ec68887965b0846bf8
Elbaz <eran.elbaz@env0.com>
2022-02-20 14:39:45 +0200

    move endgroup

--- src/install-pnpm/index.ts
+++ src/install-pnpm/index.ts
@@ -1,32 +1,34 @@
-import { setFailed, startGroup, endGroup } from '@actions/core'
-import { Inputs } from '../inputs'
+import {setFailed, startGroup, endGroup} from '@actions/core'
+import {Inputs} from '../inputs'
 import runSelfInstaller from './run'
 
-export { runSelfInstaller }
+export {runSelfInstaller}
 
 export async function install(inputs: Inputs) {
-  async function install() {
-    startGroup('Running self-installer...')
-    const status = await runSelfInstaller(inputs)
-    endGroup()
-    return status
-  }
-  let success = false
-  let step = 0;
-  let status;
-  for (step < 3 && !success) {
-    // Runs 5 times, with values of step 0 through 4.
-    console.log('Trying to install');
-    status = await install();
-    step++;
-    if(!status) {
-      success = true
-      console.log("yay")
+    startGroup('Running self-installer...');
+    async function install() {
+        const status = await runSelfInstaller(inputs);
+        return status;
     }
-  }
-  if (!success) {
-    return setFailed(`Something went wrong, self-installer exits with code ${status}`)
-  }
+
+    let success = false;
+    let step = 0;
+    let status;
+    for (step < 3 && !success) {
+        // Runs 5 times, with values of step 0 through 4.
+        console.log('Trying to install');
+        status = await install();
+        step++;
+        if (!status) {
+            success = true;
+            console.log("yay")
+        }
+    }
+    endGroup();
+
+    if (!success) {
+        return setFailed(`Something went wrong, self-installer exits with code ${status}`)
+    }
 }
 
 export default install
