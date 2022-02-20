d973ff226dfffd895f182d9f96782e85b7f7f876
Elbaz <eran.elbaz@env0.com>
2022-02-20 13:08:49 +0200

    retry

--- src/install-pnpm/index.ts
+++ src/install-pnpm/index.ts
@@ -5,10 +5,26 @@ import runSelfInstaller from './run'
 export { runSelfInstaller }
 
 export async function install(inputs: Inputs) {
-  startGroup('Running self-installer...')
-  const status = await runSelfInstaller(inputs)
-  endGroup()
-  if (status) {
+  async function install() {
+    startGroup('Running self-installer...')
+    const status = await runSelfInstaller(inputs)
+    endGroup()
+    return status
+  }
+  let success = false
+  let step = 0;
+  let status;
+  for (step < 3 && !success) {
+    // Runs 5 times, with values of step 0 through 4.
+    console.log('Trying to install');
+    status = await install();
+    step++;
+    if(!status) {
+      success = true
+      console.log("yay")
+    }
+  }
+  if (!success) {
     return setFailed(`Something went wrong, self-installer exits with code ${status}`)
   }
 }
